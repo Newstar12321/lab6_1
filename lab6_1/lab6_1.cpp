20a2e3acbb6f2ccebcf2228a6a8a59171f1ba5b0
Newstar12321 <tazeev.zhenya@mail.ru>
2023-12-01 22:01:50 +0700

    Добавьте файлы проекта.

---
+++ lab6_1/lab6_1.cpp
@@ -0,0 +1,49 @@
+﻿/*
+Дана матрица размера M × N. Найти максимальный среди минимальных элементов ее строк.
+*/
+
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    size_t m, n;
+    const int MAX_SIZE = 10;
+    int array[MAX_SIZE][MAX_SIZE];
+
+    cout << "Введите размер матрицы m*n: ";
+    cin >> m >> n;
+
+    if ((n < 1) or (m < 1) or (n > MAX_SIZE) or (m > MAX_SIZE))
+    {
+        cerr << "Ошибка: размер массива должен быть не меньше 1 и не больше " << MAX_SIZE;
+        return 1;
+    }
+
+    cout << "Введите элементы массива:" << endl;
+    for (size_t i = 0; i < m; i++)
+    {
+        for (size_t j = 0; j < n; j++)
+        {
+            cin >> array[i][j];
+        }
+    }
+
+    int min = array[0][0];
+    int max = min;
+    for (size_t i = 0; i < m; i++)
+    {
+        min = array[i][0];
+        for (size_t j = 0; j < n; j++)
+        {
+            if (array[i][j] < min) min = array[i][j];
+        }
+        if (max < min) max = min;
+    }
+
+    cout << max;
+}
